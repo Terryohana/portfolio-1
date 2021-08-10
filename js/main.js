4b4aaaac017defdc74dddb3587d6573d9fb6e39d
camipeludero <camipeludero@gmail.com>
2021-08-10 10:54:20 -0300

    almost ready

--- js/main.js
+++ js/main.js
@@ -9,4 +9,20 @@ document.addEventListener('DOMContentLoaded', function(){
             hamburger.classList.toggle('open');
         }
         
+        const swiper = new Swiper('.swiper-container', {
+            // Optional parameters
+            direction: 'horizontal',
+            loop: true,
+          
+            // Navigation arrows
+            navigation: {
+              nextEl: '.swiper-button-next',
+              prevEl: '.swiper-button-prev',
+            },
+          
+            // And if we need scrollbar
+            scrollbar: {
+              el: '.swiper-scrollbar',
+            },
+          });
     });
